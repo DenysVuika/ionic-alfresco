8d99a494b58d00baa8250d7c34085eb92485a9dd
Denys Vuika <denis.vuyka@gmail.com>
2016-08-26 17:18:11 +0100

    Simplified TypeScript linting

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,25 @@ const del = require('del');
 const merge = require('merge2');
 // const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
-// const argv = process.argv;
-
-// compilation
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json', { sortOutput: true });
-
+// const argv = process.argv;
 // const isRelease = argv.indexOf('--release') > -1;
-
-/**
- * Ionic Gulp tasks, for more information on each see
- * https://github.com/driftyco/ionic-gulp-tasks
- *
- * Using these will allow you to stay up to date if the default Ionic 2 build
- * changes, but you are of course welcome (and encouraged) to customize your
- * build however you see fit.
- */
-const tslint = require('ionic-gulp-tslint');
+const tslint = require("gulp-tslint");
 
 gulp.task('lint', function() {
-  return tslint({
-    src: 'src/**/*.ts'
-  });
+  return gulp.src('src/**/*.ts')
+    .pipe(tslint({
+      formatter: 'prose'
+    }))
+    .pipe(tslint.report());
 });
 
 gulp.task('clean', function (){
   return del('build');
 });
 
-gulp.task('build', ['clean'], function () {
+gulp.task('build', ['clean', 'lint'], function () {
   let tsResult = tsProject.src()
     .pipe(sourcemaps.init())
     .pipe(ts(tsProject));
