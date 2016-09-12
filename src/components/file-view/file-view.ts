375e3b280b5db4a9695b9c7d786c87440634948d
Denys Vuika <denis.vuyka@gmail.com>
2016-09-12 21:15:59 +0100

    #3 improved support for text-based content viewing

--- src/components/file-view/file-view.ts
+++ src/components/file-view/file-view.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Http } from '@angular/http';
 import { MinimalNodeEntryEntity } from 'alfresco-js-api';
 import { NodeService } from '../../services/node.service';
 
 @Component({
   selector: 'alf-file-view',
   template: `
-    <div *ngIf="contentUrl" [ngSwitch]="mimeType">
+    <div *ngIf="contentUrl && !textContent" [ngSwitch]="mimeType">
       <div *ngSwitchCase="'video/mp4'">
         <video controls autoplay class="video-player">
           <source [src]="contentUrl" type="video/mp4">
@@ -25,6 +26,9 @@ import { NodeService } from '../../services/node.service';
         <pre *ngIf="debug">{{node | json}}</pre>
       </div>
     </div>
+    <div *ngIf="textContent">
+      <pre>{{textContent}}</pre>
+    </div>
   `
 })
 export class FileViewComponent implements OnInit {
@@ -38,8 +42,11 @@ export class FileViewComponent implements OnInit {
   node: MinimalNodeEntryEntity;
   mimeType: string;
   contentUrl: string;
+  textContent: string;
 
-  constructor(private nodeService: NodeService) {
+  constructor(
+    private nodeService: NodeService,
+    private http: Http) {
   }
 
   ngOnInit() {
@@ -50,7 +57,28 @@ export class FileViewComponent implements OnInit {
         if (node && node.content && node.content.mimeType) {
           this.mimeType = node.content.mimeType;
           this.contentUrl = this.nodeService.getContentUrl(this.nodeId);
+          if (this.isTextFile(this.mimeType)) {
+            this.loadTextContent(this.contentUrl);
+          }
         }
+      });
+  }
+
+  private isTextFile(mimeType: string): boolean {
+    return [
+      'text/plain',
+      'text/csv',
+      'text/xml',
+      'application/json',
+      'application/x-javascript'
+    ].indexOf(mimeType) > -1;
+  }
+
+  private loadTextContent(url: string) {
+    this.http.get(url).subscribe(
+      res => {
+        console.log(res);
+        this.textContent = res.text();
       });
   }
 
